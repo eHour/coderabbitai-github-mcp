ebf3624b4bed8b7dfe593550399ed3efc59eeaa0
Thies Edeling <thies@te-con.nl>
2025-09-06 10:46:24 +0200

    dist removed

--- dist/config/loader.d.ts
+++
@@ -1,4 +0,0 @@
-import { Config } from '../types/index.js';
-export declare function loadConfig(): Config;
-export declare function validateGitHubToken(config: Config): void;
-//# sourceMappingURL=loader.d.ts.map
