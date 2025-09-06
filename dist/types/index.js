d8608baad9c60aff6704c234d83f4ca2ad4c060c
Thies Edeling <thies@getehour.com>
2025-09-06 16:53:19 +0200

    chore: remove dist from version control and add to gitignore
    Built files should not be tracked in git.

--- dist/types/index.js
+++
@@ -1,8 +0,0 @@
-export var ValidationResult;
-(function (ValidationResult) {
-    ValidationResult["VALID"] = "valid";
-    ValidationResult["INVALID"] = "invalid";
-    ValidationResult["NEEDS_REVIEW"] = "needs_review";
-    ValidationResult["UNPATCHABLE"] = "unpatchable";
-})(ValidationResult || (ValidationResult = {}));
-//# sourceMappingURL=index.js.map
